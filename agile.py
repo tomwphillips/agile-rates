a6d0c05d0696cdee3c8de850af675b712837fd97
Tom Phillips <tom@tomwphillips.co.uk>
2023-04-09 21:25:35 +0100

    Get tariffs

--- agile.py
+++ agile.py
@@ -1,11 +1,19 @@
-import marshmallow
+from dataclasses import dataclass
+
+import jq
 import requests
 
 API_BASE_URL = "https://api.octopus.energy/v1"
 
 
-class ProductSchema(marshmallow.Schema):
-    code = marshmallow.fields.String()
+@dataclass
+class Product:
+    code: str
+
+
+@dataclass
+class Tariff:
+    code: str
 
 
 def list_products():
@@ -14,6 +22,38 @@ def list_products():
     if decoded_response.get("next"):
         raise NotImplementedError("paginated response")
 
-    return ProductSchema(many=True, unknown=marshmallow.EXCLUDE).load(
-        decoded_response["results"]
-    )
+    return [
+        Product(**product)
+        for product in (
+            jq.compile(
+                '.results [] | select((.code | startswith("AGILE")) and .brand == "OCTOPUS_ENERGY" and .direction == "IMPORT") | {code: .code}'
+            )
+            .input(decoded_response)
+            .all()
+        )
+    ]
+
+
+def get_tariffs(product):
+    response = requests.get(API_BASE_URL + "/products/" + product.code)
+    decoded_response = response.json()
+    return [
+        Tariff(**tariff)
+        for tariff in (
+            jq.compile(
+                ".single_register_electricity_tariffs[] | {code: .direct_debit_monthly.code}"
+            )
+            .input(decoded_response)
+            .all()
+        )
+    ]
+
+
+if __name__ == "__main__":
+    products = list_products()
+
+    tariffs = []
+    for product in products:
+        tariffs.extend(get_tariffs(product))
+
+    print(tariffs)
--- test_agile.py
+++ test_agile.py
@@ -1,6 +1,6 @@
 import responses
 
-from agile import API_BASE_URL, list_products
+from agile import API_BASE_URL, Tariff, get_tariffs, list_products, get_tariffs, Product
 
 
 @responses.activate()
@@ -9,13 +9,30 @@ def test_list_products():
         API_BASE_URL + "/products/",
         json={
             "results": [
-                {"code": "PRODUCT-1", "full_name": "Product 1"},
-                {"code": "PRODUCT-2", "full_name": "Product 2"},
+                {"code": "AGILE-1", "brand": "OCTOPUS_ENERGY", "direction": "IMPORT"},
+                {"code": "AGILE-2", "brand": "STARFISH_ENERGY"},
+                {"code": "STIFF-1", "brand": "OCTOPUS_ENERGY"},
             ]
         },
     )
     products = list_products()
-    assert products == [
-        {"code": "PRODUCT-1"},
-        {"code": "PRODUCT-2"},
+    assert products == [Product(code="AGILE-1")]
+
+
+@responses.activate()
+def test_get_tariffs():
+    responses.get(
+        API_BASE_URL + "/products/PRODUCT-1",
+        json={
+            "code": "PRODUCT-1",
+            "single_register_electricity_tariffs": {
+                "_A": {"direct_debit_monthly": {"code": "A-1"}},
+                "_B": {"direct_debit_monthly": {"code": "B-1"}},
+            },
+        },
+    )
+    tariffs = get_tariffs(Product(code="PRODUCT-1"))
+    assert tariffs == [
+        Tariff(code="A-1"),
+        Tariff(code="B-1"),
     ]
