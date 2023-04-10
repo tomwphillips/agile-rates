3fea1c3fa4103231593e59ff69c7277d72a46e59
Tom Phillips <tom@tomwphillips.co.uk>
2023-04-10 20:20:26 +0100

    Get unit rates

--- agile.py
+++ agile.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 import jq
 import requests
@@ -14,6 +15,23 @@ class Product:
 @dataclass
 class Tariff:
     code: str
+    product_code: str
+
+
+@dataclass
+class UnitRate:
+    valid_from: datetime
+    valid_to: datetime
+    value_exc_vat: float
+    value_inc_vat: float
+
+    def from_decoded_json(**kwargs):
+        return UnitRate(
+            valid_from=datetime.fromisoformat(kwargs["valid_from"]),
+            valid_to=datetime.fromisoformat(kwargs["valid_to"]),
+            value_exc_vat=kwargs["value_exc_vat"],
+            value_inc_vat=kwargs["value_inc_vat"],
+        )
 
 
 def list_products():
@@ -38,10 +56,31 @@ def get_tariffs(product):
     response = requests.get(API_BASE_URL + "/products/" + product.code)
     decoded_response = response.json()
     return [
-        Tariff(**tariff)
+        Tariff(**tariff, product_code=product.code)
         for tariff in (
             jq.compile(
                 ".single_register_electricity_tariffs[] | {code: .direct_debit_monthly.code}"
+            )
+            .input(decoded_response)
+            .all()
+        )
+    ]
+
+
+def get_unit_rates(tariff):
+    response = requests.get(
+        API_BASE_URL
+        + f"/products/{tariff.product_code}/electricity-tariffs/{tariff.code}/standard-unit-rates/"
+    )
+    decoded_response = response.json()
+    if decoded_response.get("next"):
+        raise NotImplementedError("paginated response")
+
+    return [
+        UnitRate.from_decoded_json(**unit_rate)
+        for unit_rate in (
+            jq.compile(
+                ".results[] | {valid_from: .valid_from, valid_to: .valid_to, value_exc_vat: .value_exc_vat, value_inc_vat: .value_inc_vat}"
             )
             .input(decoded_response)
             .all()
@@ -56,4 +95,5 @@ if __name__ == "__main__":
     for product in products:
         tariffs.extend(get_tariffs(product))
 
-    print(tariffs)
+    for tariff in tariffs:
+        print(tariff)
--- test_agile.py
+++ test_agile.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timezone
+
 import responses
 
-from agile import API_BASE_URL, Tariff, get_tariffs, list_products, get_tariffs, Product
+from agile import (API_BASE_URL, Product, Tariff, UnitRate, get_tariffs,
+                   get_unit_rates, list_products)
 
 
 @responses.activate()
@@ -33,6 +36,48 @@ def test_get_tariffs():
     )
     tariffs = get_tariffs(Product(code="PRODUCT-1"))
     assert tariffs == [
-        Tariff(code="A-1"),
-        Tariff(code="B-1"),
+        Tariff(code="A-1", product_code="PRODUCT-1"),
+        Tariff(code="B-1", product_code="PRODUCT-1"),
+    ]
+
+
+@responses.activate()
+def test_get_unit_rates():
+    responses.get(
+        API_BASE_URL
+        + "/products/PRODUCT-CODE/electricity-tariffs/TARIFF-CODE/standard-unit-rates/",
+        json={
+            "count": 7724,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "value_exc_vat": 7.78,
+                    "value_inc_vat": 8.169,
+                    "valid_from": "2023-04-10T21:30:00Z",
+                    "valid_to": "2023-04-10T22:00:00Z",
+                },
+                {
+                    "value_exc_vat": 15.84,
+                    "value_inc_vat": 16.632,
+                    "valid_from": "2023-04-10T21:00:00Z",
+                    "valid_to": "2023-04-10T21:30:00Z",
+                },
+            ],
+        },
+    )
+    unit_rates = get_unit_rates(Tariff(code="TARIFF-CODE", product_code="PRODUCT-CODE"))
+    assert unit_rates == [
+        UnitRate(
+            valid_from=datetime(2023, 4, 10, 21, 30, tzinfo=timezone.utc),
+            valid_to=datetime(2023, 4, 10, 22, 0, tzinfo=timezone.utc),
+            value_exc_vat=7.78,
+            value_inc_vat=8.169,
+        ),
+        UnitRate(
+            valid_from=datetime(2023, 4, 10, 21, 0, tzinfo=timezone.utc),
+            valid_to=datetime(2023, 4, 10, 21, 30, tzinfo=timezone.utc),
+            value_exc_vat=15.84,
+            value_inc_vat=16.632,
+        ),
     ]
